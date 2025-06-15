95f2c225a58ffe5b53a6ef2abceda156183fbf14
kehuydi88895 <2022nomercy@gmail.com>
2025-06-16 01:54:03 +0700

    Add files via upload

---
+++ download_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p assets/images
+
+curl -L 'https://w.ladicdn.com/684f0da97dacc200126532ec/qsid0137_06f8df98b65b4ffe82851bcd5a011ff1_1024x1024.webp' -o assets/images/qsid0137_06f8df98b65b4ffe82851bcd5a011ff1_1024x1024.webp
+curl -L 'https://w.ladicdn.com/s750x750/684f0da97dacc200126532ec/20231223_dq9yivw0zw.jpeg' -o assets/images/20231223_dq9yivw0zw.jpeg
+curl -L 'https://w.ladicdn.com/684f0da97dacc200126532ec/z4433936132052-c5d0ead1b0f0a85144dd2805611b86c5-min.webp' -o assets/images/z4433936132052-c5d0ead1b0f0a85144dd2805611b86c5-min.webp
+curl -L 'https://w.ladicdn.com/s750x750/684f0da97dacc200126532ec/4bc317cd0ca6a6584f496d3297461301.jpg' -o assets/images/4bc317cd0ca6a6584f496d3297461301.jpg
+curl -L 'https://w.ladicdn.com/s750x750/684f0da97dacc200126532ec/ef064a8c975f2302cda91cf3ca821c98.jpg' -o assets/images/ef064a8c975f2302cda91cf3ca821c98.jpg
